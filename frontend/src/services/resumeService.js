1bd5d185520a95549a9ba59937db929af8b9eff1
RonitPatil2005 <ronitrpatil16@gamil.com>
2026-08-28 10:57:41 +0530

    update resumeservice

--- frontend/src/services/resumeService.js
+++ frontend/src/services/resumeService.js
@@ -1,23 +1,26 @@
 import axios from "axios";
 
+const API_URL =
+  import.meta.env.BACKEND_API_URL ||
+  "https://jobpilot-backend-wgv0.onrender.com";
+
 export const uploadResume = async (file) => {
+  console.log("Resume Service: Sending PDF...");
+  console.log("API URL:", API_URL);
 
   const formData = new FormData();
 
-  formData.append(
-    "resume",
-    file
-  );
+  formData.append("resume", file);
 
-  console.log(
-    "Resume Service: Sending PDF..."
+  const response = await axios.post(
+    `${API_URL}/api/resume/analyze`,
+    formData,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }
   );
-
-  const response =
-    await axios.post(
-      `${import.meta.env.VITE_API_URL}/api/resume/analyze`,
-      formData
-    );
 
   console.log(
     "Resume Service Response:",
